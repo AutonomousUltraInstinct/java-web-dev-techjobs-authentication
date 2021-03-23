851b23a3c9e2d28fbcc7b85d21287f3c31130996
AutonomousUltraInstinct <56056368+AutonomousUltraInstinct@users.noreply.github.com>
2021-03-22 20:52:31 -0500

    Add existing

---
+++ src/main/java/org/launchcode/javawebdevtechjobsauthentication/models/DTO/RegisterFormDTO.java
@@ -0,0 +1,20 @@
+package org.launchcode.javawebdevtechjobsauthentication.models.DTO;
+
+public class RegisterFormDTO extends LoginFormDTO{
+
+    private String verifyPassword;
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+    }
+
+    public String getUsername() {
+    }
+
+    public String getPassword() {
+    }
+}
